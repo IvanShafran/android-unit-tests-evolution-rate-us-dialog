0a4687f96d8313af53cb2679b33524ddddde0f27
Ivan Shafran <shafran.fun@gmail.com>
2019-08-22 13:44:04 +0300

    Decrease nesting in Spek tests

--- app/src/test/java/com/github/ivanshafran/unit_tests_evolution/v5/ShowRateUsLogicTest.kt
+++ app/src/test/java/com/github/ivanshafran/unit_tests_evolution/v5/ShowRateUsLogicTest.kt
@@ -75,34 +75,32 @@ class ShowRateUsLogicTest : Spek({
             }
         }
 
-        context("'rate us' was shown already") {
-            context("user clicked 'show me later' on the dialog") {
-                context("less than two months passed and user clicks buy") {
-                    beforeEachTest {
-                        prepareConditions(
-                            buyClickedTimes = 3,
-                            lastShownTimeMillis = SOME_DAY_IN_MILLIS,
-                            currentTimeMillis = SOME_DAY_IN_MILLIS + LESS_THAN_TWO_MONTHS
-                        )
-                    }
-
-                    it("should not show 'rate us' again") {
-                        Assert.assertFalse(showRateUsLogic.shouldShowRateUs())
-                    }
+        context("'rate us' was shown already, and user clicked 'show me later' on the dialog") {
+            context("less than two months passed and user clicks buy") {
+                beforeEachTest {
+                    prepareConditions(
+                        buyClickedTimes = 3,
+                        lastShownTimeMillis = SOME_DAY_IN_MILLIS,
+                        currentTimeMillis = SOME_DAY_IN_MILLIS + LESS_THAN_TWO_MONTHS
+                    )
                 }
 
-                context("more than two months passed and user clicks buy") {
-                    beforeEachTest {
-                        prepareConditions(
-                            buyClickedTimes = 3,
-                            lastShownTimeMillis = SOME_DAY_IN_MILLIS,
-                            currentTimeMillis = SOME_DAY_IN_MILLIS + MORE_THAN_TWO_MONTHS
-                        )
-                    }
+                it("should not show 'rate us' again") {
+                    Assert.assertFalse(showRateUsLogic.shouldShowRateUs())
+                }
+            }
 
-                    it("should show 'rate us' again") {
-                        Assert.assertTrue(showRateUsLogic.shouldShowRateUs())
-                    }
+            context("more than two months passed and user clicks buy") {
+                beforeEachTest {
+                    prepareConditions(
+                        buyClickedTimes = 3,
+                        lastShownTimeMillis = SOME_DAY_IN_MILLIS,
+                        currentTimeMillis = SOME_DAY_IN_MILLIS + MORE_THAN_TWO_MONTHS
+                    )
+                }
+
+                it("should show 'rate us' again") {
+                    Assert.assertTrue(showRateUsLogic.shouldShowRateUs())
                 }
             }
         }
